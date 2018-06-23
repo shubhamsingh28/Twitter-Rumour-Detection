8094c2348a2de11af5e97725947a1af60eab53a1
Shubham Singh <shubham.singh2882@gmail.com>
2018-06-23 13:01:50 +0530

    Python code to seperate language into english hindi and others language
    used langdetect library for separating dataset and classifying in different folder separately .

---
+++ readFolder.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 16 13:50:19 2018
+
+@author: Shubham
+"""
+
+import os
+import sys
+import codecs
+from langdetect import detect
+def process(lines,filename):
+    c=0
+    hin=0
+    eng=0
+    d=0
+    outputFile = codecs.open("otherLang/"+filename, "w+", "utf-8")
+    outputFile.write('id \t user_id \t date \t text\n')
+    #outputFile1 = codecs.open("english/"+filename, "w+", "utf-8")
+    #outputFile1.write('id \t user_id \t date \t text\n')
+    #print(len(lines))
+    for i in lines[:]:
+        if(len(i)>10):
+            s=i.split('\t')
+            try:
+                if(detect(s[3])=='hi'):
+                    #outputFile.write(i+"\n")
+                    hin=hin+1
+                elif(detect(s[3])=='en'):
+                    #outputFile1.write(i+"\n")
+                    eng=eng+1
+                else:
+                    c=c+1
+                    outputFile.write(i+"\n")
+            except:
+                d=d+1
+    
+    outputFile.close()
+    #outputFile1.close()
+    print(filename+" "+str(hin)+" "+str(eng)+" "+str(c)+" "+str(d))
+for filename in os.listdir(os.getcwd()+'\indian Rumor'):
+    #print(filename)
+    with open(os.getcwd()+"\indian Rumor/"+filename,'r', encoding="utf8") as f:
+        lines = f.readlines()
+        #print(filename+'\t\t'+str(int(len(lines)/2)))
+        process(lines,filename)
+        
+    
